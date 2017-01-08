e2c10f32125024f5965c60181ce4e262e60fd975
FlashGordon95 <ryangordon210@gmail.com>
2017-01-08 01:12:14 +0000

    Parameter validation
    In order to improve robustness I have some parameter handling now.
    If we receive no parameters we default to the default IP and file path.
    If we receive 1 parameter:
    We check if its a directory; If so we use this and default port
    We check if it is a port number; If so we use this and default filepath

--- MultiThreaded-FileServer/src/ie/gmit/sw/server/ServerRunner.java
+++ MultiThreaded-FileServer/src/ie/gmit/sw/server/ServerRunner.java
@@ -1,5 +1,8 @@
 package ie.gmit.sw.server;
 
+import java.io.File;
+import java.nio.file.Files;
+
 public class ServerRunner {
 	public static void main(String[] args) {
 		//variables for port number and file path
@@ -18,23 +21,54 @@ public class ServerRunner {
 			 * if the user did provide something but we can't use it for port || filePath
 			 * then we will close the program.
 			 * */
-			
-			
-			System.out.println("Arguements missing. Defaulting to port 7777 and downloads dir");
 //			if(args[0]==null || args[0] instanceof String){
 //				System.out.println("No port number provided");
 //			}
 			//if no parameter provided
 			if(args.length == 0){
+				System.out.println("No arguements provided. Defaulting to port 7777 and desktop/files dir");
 				String home = System.getProperty("user.home"); //get the home directory of the user
 				port = 7777; //the default value for server
-				filePath = home+"/Desktop/files"; //the default directory is the desktop folder of the server. 
+				filePath = "./server-files"; //the default directory is the desktop folder of the server. 
 			}
+			/*
+			 * If we only get one parameter, we will check it and see if we can work with it
+			 */
+			else if(args.length ==1){
+				System.out.println("Only 1 parameter provided");
+				File file = new File(args[0]);
+				//check if the parameter given is a directory
+				if(file.isDirectory()){
+					System.out.println("Parameter appears to be a directory. Using default port number and this directory");
+					filePath = args[0];
+					port = 7777; //the default value for server
+				}
+				//also check if the parameter is an int, it could be a port number
+				else if(isInteger(args[0])){
+					System.out.println("Parameter appears to be a port number. Using default directory and this number");
+
+					//TODO maybe some more checking if its a valid port
+					String home = System.getProperty("user.home");
+					filePath = "./server-files"; //the default directory is the desktop folder of the server. 
+					port = Integer.parseInt(args[0]);
+				}
+			}
+				
 			else{
 				return;
 			}
 		}
 		new WebServer(port, filePath);
 	} //end main()
-
+	public static boolean isInteger(String s) {
+	    try { 
+	        Integer.parseInt(s); 
+	    } catch(NumberFormatException e) { 
+	        return false; 
+	    } catch(NullPointerException e) {
+	        return false;
+	    }
+	    // only got here if we didn't return false
+	    return true;
+	}
 }
